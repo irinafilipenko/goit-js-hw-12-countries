489ec032bd8fd791864a8128bba6e16f75260281
irinafilipenko <73819257+irinafilipenko@users.noreply.github.com>
2021-05-12 23:39:12 +0300

    js

--- goit-js-hw-12-countries/src/index.js
+++ goit-js-hw-12-countries/src/index.js
@@ -1,1 +1,41 @@
 import './styles.css';
+import cardTemplate from './templates/countrie.hbs';
+
+var debounce = require('lodash.debounce');
+
+const cardContainerEl = document.querySelector('.cardContainer');
+const inputEl = document.querySelector('.input');
+
+inputEl.addEventListener('input', onInputSearch);
+const name = '';
+
+function onInputSearch(e) {
+    e.preventDefault();
+    
+    const searchQuery = e.currentTarget.value;
+    console.log(searchQuery);
+
+ fetchCountries(searchQuery)
+    .then(renderCounriesCard)
+    .catch(error => console.log(console.error()));
+}
+
+ 
+
+
+
+function fetchCountries(name) {
+  return    fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(data => {
+    return data.json()
+    })
+    // .then(renderCounriesCard)
+    // .catch(error => {
+    //     console.log(error)
+    // });
+  }
+
+function renderCounriesCard(country) {
+const markup = cardTemplate(country);
+        console.log(markup);
+        cardContainerEl.insertAdjacentHTML('beforeend', markup);
+}
